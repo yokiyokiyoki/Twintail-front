a0245ba394143e852e003b0a4f31473c2fe0b708
youjie <youjie@hudongpai.com>
2018-07-24 21:12:59 +0800

    样式布局

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="home-container">
         <div class="home-container-header"></div>
+        <div class="home-container-body">
+            <div class="home-container-body-top"></div>
+            <div class="home-container-body-bottom"></div>
+        </div>
+        <div class="home-container-footer"></div>
     </div>
 </template>
 <script>
