b9bbfe2291842632f65c4f950f1005d6837f2087
youjie <youjie@hudongpai.com>
2018-07-24 21:49:52 +0800

    样式提交

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,22 @@
 <template>
     <div class="home-container">
-        <div class="home-container-header"></div>
+        <div class="home-container-header">
+            <div class="home-container-header-left"></div>
+            <div class="home-container-header-right"></div>
+        </div>
         <div class="home-container-body">
-            <div class="home-container-body-top"></div>
-            <div class="home-container-body-bottom"></div>
+            <div class="home-container-body-top">
+                <div class="home-container-body-top-carousel"></div>
+                <div class="home-container-body-top-boxContainer clearfix" >
+                    <div class="home-container-body-top-boxContainer-box" v-for='n in 5' :key='n'>
+                        <div class="home-container-body-top-boxContainer-box-pic"></div>
+                        <div class="home-container-body-top-boxContainer-box-txt"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="home-container-body-bottom">
+
+            </div>
         </div>
         <div class="home-container-footer"></div>
     </div>
