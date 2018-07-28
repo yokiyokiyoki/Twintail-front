f76212226eee5c3b3132e2713414b6e728967c11
youjie <youjie@hudongpai.com>
2018-07-28 15:55:05 +0800

    clearfix全局样式

--- src/components/info.vue
+++ src/components/info.vue
@@ -5,7 +5,16 @@
             <div class="info-container-header-right"></div>
         </div>
         <div class="info-container-body">
-            
+            <div class="info-container-body-top">
+                <div class="info-container-body-top-left"></div>
+                <div class="info-container-body-top-right"></div>
+            </div>
+            <div class="info-container-body-middle clearfix">
+                <div class="info-container-body-middle-box"></div>
+                <div class="info-container-body-middle-box"></div>
+                <div class="info-container-body-middle-box"></div>
+                <div class="info-container-body-middle-box"></div>
+            </div>
         </div>
     </div>
 </template>
