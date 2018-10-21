2dc74b3601536b055df8f6d0294d64889cbf850e
youjie <youjie@hudongpai.com>
2018-10-21 18:12:26 +0800

    feat(时间): 格式化

--- src/components/admin/album/index.vue
+++ src/components/admin/album/index.vue
@@ -29,16 +29,20 @@
                         >
                         </el-table-column>
                         <el-table-column
-                        prop="creatAt"
+                        
                         label="创建时间"
                         >
+                        <template slot-scope="scope">
+                            <i class="el-icon-time"></i>
+                            <span style="margin-left: 10px">{{ $moment(+scope.row.creatAt).format('YYYY-MM-DD hh:mm:ss') }}</span>
+                        </template>
                         </el-table-column>
                         <el-table-column
                         fixed="right"
                         label="操作"
                         width="100">
                         <template slot-scope="scope">
-                            <!-- <el-button @click="handleEit(scope.row)" type="text" size="small">编辑</el-button> -->
+                            <el-button @click="handleEit(scope.row)" type="text" size="small">编辑</el-button>
                             <el-button @click="handleDelete(scope.row,scope.$index)" type="text" size="small">删除</el-button>
                         </template>
                         </el-table-column>
