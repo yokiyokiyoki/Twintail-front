5c10c5b92760079a475f0c683a200fb0e9480585
youjie <youjie@hudongpai.com>
2018-10-31 22:10:06 +0800

    build

--- src/components/pc/info.vue
+++ src/components/pc/info.vue
@@ -4,7 +4,7 @@
         <div class="info-container-body">
             <div class="info-container-body-top">
                 <div class="info-container-body-top-left" v-if='info.people'>
-                    <img :src="info.people.tx_pic" style="max-width:100%;" >
+                    <img :src="info.people.tx_pic" style="height:100%;max-width:100%;" >
                 </div>
                 <div class="info-container-body-top-right" v-if='info.people'>
                     <div class="info-container-body-top-right-name">{{info.people.username}}</div>
@@ -17,7 +17,7 @@
                 <div class="info-container-body-middle-box" v-for='(item,index) in info.albums' :key='index' @click="handleClickItem(item)">
                     <div class="info-container-body-middle-box-container">
                         <div class="info-container-body-middle-box-container-pic">
-                            <img :src="item.photo[0].photo_url" style="height:100%;width:100%;" v-if='item.photo.length'>
+                            <img :src="item.photo[0].photo_url" style="width:100%;" v-if='item.photo.length'>
                         </div>
                         <div class="info-container-body-middle-box-container-bottom clearfix">
                             <div class="info-container-body-middle-box-container-bottom-left">{{item.info.album_name}}</div>
