6f22e0cb1c3511941ba1754d0f0a8f0948505165
youjie <youjie@hudongpai.com>
2018-11-06 21:12:26 +0800

    fix(首页): name过长
    限制宽度溢出。。。

--- config/index.js
+++ config/index.js
@@ -11,11 +11,11 @@ module.exports = {
     assetsPublicPath: "/",
     proxyTable: {
       "/api": {
-        target: "http://127.0.0.1:7001", // 接口的域名
+        target: "http://shuangmawei.cn", // 接口的域名
         changeOrigin: true // 如果接口跨域，需要进行这个参数配置
       },
       "/public": {
-        target: "http://127.0.0.1:7001", // 接口的域名
+        target: "http://shuangmawei.cn", // 接口的域名
         changeOrigin: true // 如果接口跨域，需要进行这个参数配置
       }
     },
